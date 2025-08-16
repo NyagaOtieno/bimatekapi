cde17a00e097213522fb3e0f8434f18c97f47517
Kento <nyagaotieno@gmail.com>
2025-08-16 13:02:09 +0300

    Fix: import productValidationSchema in quotes.routes.js

--- src/routes/quotes.routes.js
+++ src/routes/quotes.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const quoteController = require("../../controllers/quoteController");
-const { productValidationSchema } = require("../../validations/product.validation");
+const { productValidationSchema } = require("../../validation/product.validation");
 
 // ========================
 // QUOTE ROUTES
