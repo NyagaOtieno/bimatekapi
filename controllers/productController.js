4a563410bb496f73506f61de574d45cc77078d5a
Kento <nyagaotieno@gmail.com>
2025-08-16 11:16:44 +0300

    Initial commit with controllers, validations, and src setup

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 const { PrismaClient, CoverageType } = require('@prisma/client');
 const prisma = new PrismaClient();
-const productValidationSchema = require('../validations/productValidation');
+const { productSchema } = require('../validations/productValidation');
+
 
 // Normalize coverage strings
 function normalizeCoverage(value) {
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const productController = require("../controllers/productController");
+const productController = require('../../controllers/productController');
+
 
 // ========================
 // Product Routes
